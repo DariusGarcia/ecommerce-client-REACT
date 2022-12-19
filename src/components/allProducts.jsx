35f21082770a3ced5aef440a4e6817b15e0eb850
Darius Garcia <dariusgarcia888@gmail.com>
2022-12-18 23:20:07 -0800

    added fetch request for all products

--- src/components/allProducts.jsx
+++ src/components/allProducts.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const AllProducts = () => {
-	return <div>all products</div>
+	const [allProducts, setAllProducts] = useState([])
+
+	useEffect(() => {
+		function fetchAllProducts() {
+			fetch('http://localhost:3001/api/product')
+				.then((res) => res.json())
+				.then((products) => setAllProducts(products))
+				.catch((err) => console.error(err))
+		}
+
+		fetchAllProducts()
+		console.log(allProducts)
+	}, [])
+	return (
+		<section className='all-products-container'>
+			<nav className='all-products-nav'>
+				<ul>
+					{allProducts.map((item) => {
+						return item ? (
+							<li className='all-products-nav-items'>
+								<h3>{item.name}</h3>
+								<p>{item.owner}</p>
+							</li>
+						) : null
+					})}
+				</ul>
+			</nav>
+			<section className='all-products-section'></section>
+		</section>
+	)
 }
 
 export default AllProducts
