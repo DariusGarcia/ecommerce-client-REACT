1a0308504b7481c7e2ddbcdecf8a154d6a9ed0b7
Darius Garcia <dariusgarcia888@gmail.com>
2022-12-20 11:04:30 -0800

    added custom hook to fetch data

--- src/components/singleProduct.jsx
+++ src/components/singleProduct.jsx
@@ -1,60 +1,49 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import image from '../assets/images/japanese.png'
+import useFetchData from '../hooks/useFetchData'
 import Footer from './Footer'
 import SideNav from './SideNav'
 import SimilarProducts from './similarProducts'
 
 const SingleProduct = () => {
-	const { id } = useParams()
-	const [product, setProduct] = useState([])
-
-	useEffect(() => {
-		function fetchSingleProduct() {
-			const productID = id
-			const baseURL = 'http://localhost:3001'
-			const singleProductURL = `${baseURL}/api/product/${productID}`
-			fetch(singleProductURL)
-				.then((res) => res.json())
-				.then((products) => setProduct(products))
-				.catch((err) => console.error(err))
-			console.log(
-				'\n🚀 ~ file: SingleProduct.jsx:21 ~ fetchSingleProduct ~ SINGLE PRODUCT: ',
-				setProduct
-			)
-		}
-		fetchSingleProduct()
-	}, [])
+	const productID = '3'
+	const url = `http://localhost:3001/api/product/${productID}`
+	const { data, isLoading, error } = useFetchData(url)
 
 	return (
 		<>
-			<section className='all-products-container'>
-				<div className='all-products-div'>
-					{/* side navigation bar */}
-					<SideNav title={product.product_name} />
+			{isLoading ? (
+				<h1 className='loading'>Loading</h1>
+			) : (
+				<section className='all-products-container'>
+					<div className='all-products-div'>
+						{/* side navigation bar */}
+						<SideNav title={data.product_name} />
 
-					{/* single product section */}
-					<section className='single-product-section'>
-						<ul>
-							<li key={product.id}>
-								<article
-									key={product.id}
-									id={product.id}
-									className='all-products-item'
-								>
-									<img alt='product' src={image}></img>
-									<h3 className='product-name'>
-										{product.product_name ? product.product_name : 'NOT FOUND'}
-									</h3>
-									<p className='product-description'>{product.description}</p>
-									<button className='all-products-btn'>Add to cart</button>
-								</article>
-								<SimilarProducts />
-							</li>
-						</ul>
-					</section>
-				</div>
-			</section>
+						{/* single product section */}
+						<section className='single-product-section'>
+							<ul>
+								<li key={data.id}>
+									<article
+										key={data.id}
+										id={data.id}
+										className='all-products-item'
+									>
+										<img alt='product' src={data.image_url}></img>
+										<h3 className='product-name'>
+											{data.product_name ? data.product_name : 'NOT FOUND'}
+										</h3>
+										<p className='product-description'>{data.description}</p>
+										<button className='all-products-btn'>Add to cart</button>
+									</article>
+									<SimilarProducts />
+								</li>
+							</ul>
+						</section>
+					</div>
+				</section>
+			)}
 			<Footer />
 		</>
 	)
