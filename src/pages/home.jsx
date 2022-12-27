f2e876f7a766ae830bd7ba6de1d32a16fc0970bb
Darius Garcia <dariusgarcia888@gmail.com>
2022-12-26 22:27:46 -0800

    added section to home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import HomeComp from '../components/Home'
 import Footer from '../components/Footer'
+import LandingHeader from '../components/landingHeader'
 
 const Home = () => {
 	return (
 		<div>
 			<Navbar />
+			<LandingHeader />
 			<HomeComp />
 			<Footer />
 		</div>
